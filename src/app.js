47f126ff976f97940da8f73e6083d493ca85d4c4
Nick D'hondt <nick.dhondt@student.howest.be>
2016-11-24 15:22:38 +0100

    Commit

--- src/app.js
+++ src/app.js
@@ -1,10 +1,10 @@
 var express = require("express");
 var app = express();
 
-var gcmtest = require("./controllers/gcmtestcontroller");
+var gcmtest = require("./backend/controllers/gcmtestcontroller");
 
 app.use("/gcmtest", gcmtest);
 
-app.listen(/*process.env.PORT || */3000, function(){
+app.listen(process.env.PORT || 3000, function(){
     console.log("Listening on port 3000");
 });
--- src/backend/controllers/gcmtestcontroller.js
+++ src/backend/controllers/gcmtestcontroller.js
@@ -11,14 +11,14 @@ router.get("/", function (req, res) {
 });
 
 router.post("/", function (req, res) {
-    var body = JSON.stringify({condition: "'" + req.body.topic + "' in topics", notification:{title: req.body.title, body: req.body.body}});
+    var body = JSON.stringify({condition: "'" + req.body.topic + "' in topics", notification:{title: req.body.title, body: req.body.body, sound:"default"}});
 
     var options = {
         url: "https://fcm.googleapis.com/fcm/send",
         method: "POST",
         headers: {
             "Content-Type": "application/json",
-            "Authorization": "key=AAAADThleZQ:APA91bGw5-Al9JrL7lY2QUOJYNjVUkZu1OnM15Y5NP-fzwYmhL0kVi6cO5tdJtyJg0vrdbdrWDLC7ELvWF0vL7UtVWIGpspZiCiHE68viwiWkCFaKyvE77Up-QCb026rr6VEFeU90Y2Z2Pvm7ajz-5Xi5Ov33bPVKA",
+            "Authorization": "key=AAAADThleZQ:APA91bGw5-Al9JrL7lY2QUOJYNjVUkZu1OnM15Y5NP-fzwYmhL0kVi6cO5tdJtyJg0vrdbdrWDLC7ELvWF0vL7UtVWIGpspZiCiHE68viwiWkCFaKyvE77Up-QCb026rr6VEFeU90Y2Z2Pvm7ajz-5Xi5Ov33bPVKA"
         },
         body: body
     };
