545529d1c90708d0dafada56ce6ca8fa6eac3c2f
steven <steven.mollie@hotmail.com>
2016-11-28 22:24:17 +0100

    connection with db

--- src/backend/database/db.js
+++ src/backend/database/db.js
@@ -1,12 +1,12 @@
 /**
- * Created by steve on 28/11/2016.
+ * Created by steven on 28/11/2016.
  */
 var mongoose = require('mongoose');
 
 var options = { server: { socketOptions: { keepAlive: 300000, connectTimeoutMS: 30000 } },
     replset: { socketOptions: { keepAlive: 300000, connectTimeoutMS : 30000 } } };
 
-var mongodbUri = 'mongodb://user:pass@host:port/db';
+var mongodbUri = 'mongodb://admin:abc123@ds163667.mlab.com:63667/heroku_s3b0kwzb';
 
 mongoose.connect(mongodbUri, options);
 var conn = mongoose.connection;
@@ -15,4 +15,6 @@ conn.on('error', console.error.bind(console, 'connection error:'));
 
 conn.once('open', function() {
     // Wait for the database connection to establish, then start the app.
+
+    console.log("connection established");
 });
