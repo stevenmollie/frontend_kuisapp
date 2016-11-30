4a656a7e0f1a753f4aa16f5976e77b8c63bcaa5c
steven <steven.mollie@hotmail.com>
2016-11-30 19:41:55 +0100

    HouseholdModel complete

---
+++ app/models/Household.ts
@@ -0,0 +1,58 @@
+import {User} from "./User";
+import {Task} from "./Task";
+/**
+ * Created by steve on 30/11/2016.
+ */
+
+
+export class Household{
+    private _address: string;
+
+    get address():string{
+        return this._address;
+    }
+
+    set address(address:string){
+        this._address = address;
+    }
+
+    private _phoneNumber: string;
+
+    get phoneNumber():string{
+        return this._phoneNumber;
+    }
+
+    set phoneNumber(phoneNumber:string){
+        this._phoneNumber = phoneNumber;
+    }
+
+    private _users: User[];
+
+    get users(): User[]{
+        return this._users;
+    }
+
+    set users(users: User[]){
+        this._users = users;
+    }
+
+    private _tasks: Task[];
+
+    get tasks():Task[]{
+        return this._tasks;
+    }
+
+    set tasks(tasks: Task[]){
+        this._tasks = tasks;
+    }
+
+    private _tasksTodo: Task[];
+
+    get tasksTodo():Task[]{
+        return this._tasksTodo;
+    }
+
+    set tasksTodo(tasksTodo: Task[]){
+        this._tasksTodo = tasksTodo;
+    }
+}
